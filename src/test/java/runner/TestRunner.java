2e4ff24cc7ed1eab696f97656fc83a7150ee653a
Amit Tyagi <amit.tyagi@gspann.com>
2020-06-18 17:19:25 +0530

    Code updated

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -14,7 +14,8 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(tags= {"@smartGroup"},glue = "com.sephora.msl.steps", plugin = { "json:target/json-cucumber-reports/cukejson.json",
 		"com.sephora.msl.base.BaseConfiguration","testng:target/testng-cucumber-reports/cuketestng.xml" }, features = "src/test/resources/features")
 @Test
-public class TestRunner extends AbstractTestNGCucumberParallelTests {
+public class TestRunner //extends AbstractTestNGCucumberParallelTests 
+{
 
 	private static long duration;
 
